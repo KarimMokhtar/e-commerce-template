c415d7c30eb5f3c2189274e1a16efb6a592dfb21
karim mokhtar <karimmokhtar28@gmail.com>
2020-08-03 23:10:57 +0200

    adding header and styling with links to home and shop page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shope/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 }
 
