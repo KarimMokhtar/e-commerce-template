caff4bc248ec5cdb57198dda6791a9292d5a966c
karim mokhtar <karimmokhtar28@gmail.com>
2020-08-15 16:14:10 +0200

    setting up redux and create user reducer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "./header.scss";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
