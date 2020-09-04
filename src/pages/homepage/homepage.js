a2194519667d9af674b3b0a8dd8851b3ed20d09c
karim mokhtar <karimmokhtar28@gmail.com>
2020-09-04 15:58:59 +0200

    converting home page to css in js with styled components

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./homepage.scss";
 import Directory from "../../components/directory/directory";
 
+import { HomePageContainer } from "./homepage.styles";
+
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
